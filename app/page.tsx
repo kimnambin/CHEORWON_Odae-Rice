84c4adb9f53279f3732d95504e3014cd2d539c38
kimnambin <mkkim044@gmail.com>
2026-02-24 21:35:56 +0900

    test : 커밋이 잘되는지 테스트2

--- app/page.tsx
+++ app/page.tsx
@@ -11,12 +11,11 @@ const mockProducts = [
     imageUrl: '',
   },
 ];
-d;
 
 export default function Home() {
   return (
     <main className="container mx-auto py-10">
-      <h2 className="text-2xl font-bold mb-6">인기 쌀 상품</h2>
+      <h2 className="text-2xl font-bold mb-6">인기 쌀 상품~</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         {mockProducts.map(product => (
           <ProductCard key={product.id} product={product} />
